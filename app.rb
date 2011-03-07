47d54117206ce4d4711f0f98d477dc7adc858242
Roger Braun <davinelulinvega@gmail.com>
2011-03-07 02:22:12 +0100

    works better

--- app.rb
+++ app.rb
@@ -6,15 +6,21 @@ require "./dump_joysound.rb"
 
 get "/" do
   if params[:title] then
+    
     @results = Keyword.all(:keyword.like => "#{params[:title]}%").map(&:songs).flatten.uniq
-    unless SearchProgress.first(:keyword => params[:title], :finished => false) or params[:ongoing]
-      Thread.new {
-        Joysound.search_by_title(params[:title], true, 5)
-      }
-      @ongoing = true  
-    else
-      
+    puts  "results"
+
+    if not @results.empty? then
       @ongoing = SearchProgress.first(:keyword => params[:title], :finished => false)
+    else
+      if params[:ongoing]
+        @ongoing = !SearchProgress.first(:keyword => params[:title], :finished => true)
+      else
+        Thread.new {
+          Joysound.search_by_title(params[:title], true, 5)
+        }
+        @ongoing = true  
+      end
     end
       
   end
