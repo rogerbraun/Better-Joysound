261e360205877c2955d6683b50535c6f4af8fa47
Roger Braun <davinelulinvega@gmail.com>
2011-03-07 03:00:09 +0100

    works better

--- dump_joysound.rb
+++ dump_joysound.rb
@@ -12,11 +12,11 @@ class Song
   include DataMapper::Resource
 
   property :id,   Serial
-  property :title, String
-  property :artist, String
+  property :title, String, :length => 256
+  property :artist, String, :length => 256
   property :number, String, :unique => true
-  property :genre, String
-  property :utaidashi, String
+  property :genre, String, :length => 256
+  property :utaidashi, String, :length => 256
 
   has n, :keywords, :through => Resource
 
